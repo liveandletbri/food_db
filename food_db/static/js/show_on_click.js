c18645307b98c7624df1b78892fa1986ce77fb9d
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2024-09-10 15:15:32 -0700

    new buttons and scripts

--- food_db/static/js/show_on_click.js
+++ food_db/static/js/show_on_click.js
@@ -1,6 +1,28 @@
-function showOnClick(){
+let ingredParserTextbox = document.querySelector("#ingred-parser-textbox")
+
+function showTagFormOnClick(){
     document.getElementById('add-tag-form').className="show";
 }
-function hideOnClick(){
+function hideTagFormOnClick(){
     document.getElementById('add-tag-form').className="hide";
+}
+
+function showIngredientParserOnClick(){
+    document.getElementById('ingred-form').className="hide";
+    document.getElementById('ingred-parser').className="show";
+}
+async function hideIngredientParserOnClick(){
+    let response = await fetch(`http://localhost:5000/parse`, {
+        method: "GET",
+        data: ingredParserTextbox.value,
+    })
+    .then(function(response) {
+        // The response is a Response instance.
+        return response.json();
+    })
+
+    console.log(response)
+    
+    // document.getElementById('ingred-form').className="show";
+    // document.getElementById('ingred-parser').className="hide";
 }
