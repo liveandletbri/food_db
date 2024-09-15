fc818c7ec399dbdbe3bf0e07dc6849bfb1b3c303
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2024-09-14 22:02:27 -0700

    convert cook_meal to json and write test

--- food_db/food_db_app/tests/test_models.py
+++ food_db/food_db_app/tests/test_models.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from decimal import Decimal
 from django.test import TestCase
@@ -9,11 +10,11 @@ logger = logging.getLogger(__name__)
 
 # tests for models, without needing to render a view
 
-def populate_base_data():
+def populate_base_data(title='My recipe'):
     book = RecipeBook(name='book')
     book.save()
     recipe = Recipe(
-        title = 'My recipe',
+        title = title,
         recipe_book = book,
         recipe_book_page = 22,
     )
@@ -63,8 +64,37 @@ class ModelTests(TestCase):
         logging.info(ingred)
         self.assertEqual(ingred, found_ingred)
 
-    def test_(self):
-        data = populate_base_data()
-        response = self.client.get('/cook_meal/')
+    def test_cook_meal(self):
+        populate_base_data()
+        post_data = {
+            'title': 'My recipe'
+        }
+        number_cooks = 4
+        for i in range(1, number_cooks):
+            response = self.client.post(
+                reverse('cook_meal'),
+                data=json.dumps(post_data),
+                content_type='application/json',
+            )
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.content.decode('utf-8'), str(i))
+
+    def test_cook_meal_fails_get(self):
+        response = self.client.get(reverse('cook_meal'))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_cook_meal_janky_recipe_title(self):
+        disgusting_title = """
+        \n\nThis recipe's ;janky-title: a F$#ckin! `${shart_show}`
+        """
+        populate_base_data(disgusting_title)
+        post_data = {
+            'title': disgusting_title
+        }
+        response = self.client.post(
+            reverse('cook_meal'),
+            data=json.dumps(post_data),
+            content_type='application/json',
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, '12')
+        self.assertEqual(response.content.decode('utf-8'), '1')
