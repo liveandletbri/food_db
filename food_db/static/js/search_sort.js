f08de82c3b06984da6ebdaaf658e84c532ed32ee
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2024-09-16 12:44:15 -0700

    fix sort to work on tbody

--- food_db/static/js/search_sort.js
+++ food_db/static/js/search_sort.js
@@ -1,3 +1,6 @@
+// This import is effectively done for us in the HTML, by adding it in </script> tags in main.html 
+// import { reapplyEveryOtherRowCss } from reapply_table_css
+
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
 const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
@@ -6,8 +9,10 @@ const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
 
 // do the work...
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-    const table = th.closest('table');
-    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+    let table = th.closest('table')
+    let tableBody = table.querySelector('tbody')
+    Array.from(tableBody.querySelectorAll('tr'))
         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tr => table.appendChild(tr) );
-})));
+        .forEach(tr => tableBody.appendChild(tr) )
+    reapplyEveryOtherRowCss(table.id)
+})))
