9d8807c43512eff367403ab2b420e060f9fa3695
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2024-09-16 13:06:35 -0700

    expand and comment

--- food_db/static/js/search_sort.js
+++ food_db/static/js/search_sort.js
@@ -2,9 +2,25 @@
 
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+// Returns a function responsible for sorting a specific column index 
+// (idx = columnIndex, asc = ascending order?).
+var comparer = function(idx, asc) { 
+
+    // This is used by the array.sort() function...
+    return function(a, b) { 
+
+        // This is a transient function, that is called straight away. 
+        // It allows passing in different order of args, based on 
+        // the ascending/descending order.
+        return function(v1, v2) {
+
+            // sort based on a numeric or localeCompare, based on type...
+            return (v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)) 
+                ? v1 - v2 
+                : v1.toString().localeCompare(v2);
+        }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+    }
+};
 
 // do the work...
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
