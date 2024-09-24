692a28df83e665c0558777c61338d23b41803e86
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2024-09-23 19:21:20 -0700

    form submission fix for create tag

--- food_db/static/js/create_tag.js
+++ food_db/static/js/create_tag.js
@@ -3,4 +3,8 @@ function showTagFormOnClick(){
 }
 function hideTagFormOnClick(){
     document.getElementById('add-tag-form').className="hide";
+    // form declared in recipe_add_form_validate.js
+    form.submit()
 }
+
+document.getElementById('add_tag_button').addEventListener('click', hideTagFormOnClick)
