be19598441d15c441974085bfa25e6002f9b3c4f
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-02-12 21:17:17 -0800

    a lil more s3 html cleanup

--- food_db/food_db_app/cloud_sync/parse.py
+++ food_db/food_db_app/cloud_sync/parse.py
@@ -6,14 +6,25 @@ def convert_recipe_to_html(recipe_instance, bucket_url):
     related_steps = RecipeStep.objects.filter(recipe=recipe_instance).order_by('order_number')
     
     # Create a new HTML file for the recipe
-    recipe_html = f'<html><a href="http://{bucket_url}">Back to Home</a>'
-    recipe_html += f'<head><meta name="viewport" http-equiv="Content-Type" content="text/html; charset=utf-8; width=device-width, initial-scale=1"><title>{recipe_instance.title}</title></head><body style="font-size:16pt;">'
-    recipe_html += f'<h1>{recipe_instance.title}</h1>'
+    recipe_html = f'''
+    <html><a href="http://{bucket_url}">Back to Home</a>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>{recipe_instance.title}</title>
+    </head>
+    <body style="font-size:16pt;">
+    <h1>{recipe_instance.title}</h1>'''
+
     if recipe_instance.notes != '':
         recipe_html += f'<h2>Notes</h2>{recipe_instance.notes}'
-    recipe_html += f'<h2>Ingredients</h2>'
-    recipe_html += '<table style="font-size:inherit;text-align:left;word-wrap:break-word;border-collapse:collapse;width:800;table-layout:fixed;">'
-    recipe_html += '<thead><tr><th>Ingredient</th><th>Notes</th></tr></thead>'
+    
+    recipe_html += f'''<h2>Ingredients</h2>
+    <table style="font-size:inherit;text-align:left;word-wrap:break-word;border-collapse:collapse;width:800;table-layout:fixed;">
+    <thead>
+        <tr><th>Ingredient</th><th>Notes</th></tr>
+    </thead>'''
+
     for ingredient in related_ingredients:
         recipe_html += '<tr style="border-style:solid;border-width:thin;">'
         quantity = ingredient.quantity
@@ -35,11 +46,12 @@ def convert_recipe_to_html(recipe_instance, bucket_url):
             number_units = f'{quantity_str} {unit}s '
         recipe_html += f'<td>{number_units}{food}</td>'
         recipe_html += f'<td>{ingredient.notes}</td></tr>'
-    recipe_html += '</table>'
-    recipe_html += f'<h2>Steps</h2>'
-    recipe_html += '<ol style="width:750;">'
+    recipe_html += '''</table>
+    <h2>Steps</h2>
+    <ol style="width:750;">'''
+    
     for step in related_steps:
-        recipe_html += f'<li>{step.description}</li>'
-    recipe_html += '</ol>'
-    recipe_html += '</body></html>'
+        recipe_html += f'<li style="padding:7;">{step.description}</li>'
+
+    recipe_html += '</ol></body></html>'
     return recipe_html
