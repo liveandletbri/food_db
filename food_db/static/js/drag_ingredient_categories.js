b3403ac12583697cd5a278938b91d1db2978f771
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-06-01 11:35:02 -0700

    add markdown syntax guide (closes #55)

--- food_db/static/js/drag_ingredient_categories.js
+++ food_db/static/js/drag_ingredient_categories.js
@@ -2,6 +2,7 @@ var source;
 let currentUrl = window.location.href
 let currentUrlDomain = currentUrl.split("/recipe/")[0]
 let currentRecipeKey = currentUrl.split("/recipe/")[1].split('?')[0]
+let multiplier = document.getElementById('ingredient_multiplier')
 
 async function getOrderNumber(ingredientCategory) {
     let response = await fetch(`${currentUrlDomain}/get_ingredient_category_order_number/`, {
@@ -59,7 +60,8 @@ async function handleDrop(e) {
             await swapOrderNumbers(sourceCategory, newSourceOrderNumber, targetCategory, newTargetOrderNumber)
             console.log(`swapOrderNumbers: ${sourceCategory} changed to ${newSourceOrderNumber}, ${targetCategory} changed to ${newTargetOrderNumber}`)
 
-            let response = await fetch(`/recipe/${currentRecipeKey}`, {
+            let multiplierValue = multiplier.value
+            let response = await fetch(`/recipe/${currentRecipeKey}?multiplier=${multiplierValue}`, {
                 method: "GET",
             })
             .then(function(response) {
