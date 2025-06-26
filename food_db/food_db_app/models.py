380a982f1d5b93b6411d6a48350092c4b5a1392c
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-06-25 21:36:59 -0700

    migration

--- food_db/food_db_app/models.py
+++ food_db/food_db_app/models.py
@@ -43,6 +43,7 @@ class Recipe(models.Model):
     notes = models.TextField(blank=True)
     tags = models.ManyToManyField('Tag', related_name='recipes', blank=True)
     is_baking_recipe = models.BooleanField(default=False)  # Either Cooking or Baking recipe
+    child_recipes = models.ManyToManyField('self', blank=True)
 
     # foo = models.TextField(max_length=200)
 
