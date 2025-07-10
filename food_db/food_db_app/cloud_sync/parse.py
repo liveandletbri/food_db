918176e2a68aebe42a18bbaecac89d1531a2ea30
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-07-10 07:25:17 -0700

    update s3 parse to hit child recipes (closes #62)

--- food_db/food_db_app/cloud_sync/parse.py
+++ food_db/food_db_app/cloud_sync/parse.py
@@ -2,9 +2,6 @@ def convert_recipe_to_html(recipe_instance, bucket_url):
     # Import happens here, not at top of page, so it's after Django is set up
     from food_db_app.models import Ingredient, RecipeStep
     
-    related_ingredients = Ingredient.objects.filter(recipe=recipe_instance).order_by('ingredient_category__name')
-    related_steps = RecipeStep.objects.filter(recipe=recipe_instance).order_by('order_number')
-    
     # Create a new HTML file for the recipe
     recipe_html = f'''
     <html><a href="http://{bucket_url}">Back to Home</a>
@@ -18,40 +15,68 @@ def convert_recipe_to_html(recipe_instance, bucket_url):
 
     if recipe_instance.notes != '':
         recipe_html += f'<h2>Notes</h2>{recipe_instance.notes}'
-    
-    recipe_html += f'''<h2>Ingredients</h2>
-    <table style="font-size:inherit;text-align:left;word-wrap:break-word;border-collapse:collapse;width:800;table-layout:fixed;">
-    <thead>
-        <tr><th>Ingredient</th><th>Notes</th></tr>
-    </thead>'''
 
-    for ingredient in related_ingredients:
-        recipe_html += '<tr style="border-style:solid;border-width:thin;">'
-        quantity = ingredient.quantity
-        if quantity:
-            quantity_str = str(quantity).rstrip('0').rstrip('.')
-        else:
-            quantity_str = ''
-        unit = ingredient.unit_of_measurement.name
-        food = ingredient.food.name
-        if quantity is None:
-            number_units = ''
-        elif float(quantity) == 1 and unit != '':
-            number_units = f'{quantity_str} {unit} '
-        elif float(quantity) == 1 and unit != '':
-            number_units = f'{quantity_str} {unit}s '
-        elif unit == '':
-            number_units = f'{quantity_str} '
-        else:
-            number_units = f'{quantity_str} {unit}s '
-        recipe_html += f'<td>{number_units}{food}</td>'
-        recipe_html += f'<td>{ingredient.notes}</td></tr>'
-    recipe_html += '''</table>
-    <h2>Steps</h2>
-    <ol style="width:750;">'''
-    
-    for step in related_steps:
-        recipe_html += f'<li style="padding:7;">{step.description}</li>'
+    recipe_html += '<h2>Ingredients</h2>'
 
-    recipe_html += '</ol></body></html>'
+    recipes = []
+    has_children = recipe_instance.has_children
+    if has_children:
+        recipes.extend(recipe_instance.children)
+    recipes.append(recipe_instance)
+        
+    
+    for rec in recipes:
+        if rec.has_ingredients:
+            related_ingredients = Ingredient.objects.filter(recipe=rec).order_by('ingredient_category__name')
+
+            if has_children:
+                recipe_html += f'<h3>Ingredients for {rec.title}</h3>'
+            
+            recipe_html += f'''
+            <table style="font-size:inherit;text-align:left;word-wrap:break-word;border-collapse:collapse;width:800;table-layout:fixed;">
+            <thead>
+                <tr><th>Ingredient</th><th>Notes</th></tr>
+            </thead>'''
+
+            for ingredient in related_ingredients:
+                recipe_html += '<tr style="border-style:solid;border-width:thin;">'
+                quantity = ingredient.quantity
+                if quantity:
+                    quantity_str = str(quantity).rstrip('0').rstrip('.')
+                else:
+                    quantity_str = ''
+                unit = ingredient.unit_of_measurement.name
+                food = ingredient.food.name
+                if quantity is None:
+                    number_units = ''
+                elif float(quantity) == 1 and unit != '':
+                    number_units = f'{quantity_str} {unit} '
+                elif float(quantity) == 1 and unit != '':
+                    number_units = f'{quantity_str} {unit}s '
+                elif unit == '':
+                    number_units = f'{quantity_str} '
+                else:
+                    number_units = f'{quantity_str} {unit}s '
+                recipe_html += f'<td>{number_units}{food}</td>'
+                recipe_html += f'<td>{ingredient.notes}</td></tr>'
+            recipe_html += '</table>'
+    
+    recipe_html += '<h2>Steps</h2>'
+
+    for rec in recipes:
+        if rec.has_steps:
+            related_steps = RecipeStep.objects.filter(recipe=rec).order_by('order_number')
+
+            if has_children:
+                recipe_html += f'<h3>Steps for {rec.title}</h3>'
+
+            recipe_html += '''
+            <ol style="width:750;">'''
+            
+            for step in related_steps:
+                recipe_html += f'<li style="padding:7;">{step.description}</li>'
+
+            recipe_html += '</ol>'
+
+    recipe_html += '</body></html>'
     return recipe_html
