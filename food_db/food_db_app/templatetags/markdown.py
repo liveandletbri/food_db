cc4d769168ae3af0c946036e6ba6e3db39c7ec65
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-09-23 12:04:36 -0700

    short form

--- food_db/food_db_app/templatetags/markdown.py
+++ food_db/food_db_app/templatetags/markdown.py
@@ -15,27 +15,38 @@ def markdown(value, recipe_title):
 
 def _process_ingredient_links(text, recipe_title):
     """
-    Process custom ingredient links in the format [text](!ingredient_name;ingredient_category)
+    Process custom ingredient links in the format [text](!ingredient name;ingredient category)
     and replace them with HTML spans that include tooltip data.
     """
-    # Pattern to match [text](!ingredient_name) or [text](!ingredient_name;ingredient_category)
-    pattern = r'\[([^\]]+)\]\(!([\w ]+)(;[\w ]+)?\)'
+    # Pattern to match [ingredient name] or [text](!ingredient name) or [text](!ingredient name;ingredient category)
+    pattern = r'\[([^\]]+)\](\(!([\w ]+)(;[\w ]+)?\))?'
     
     def replace_ingredient_link(match):
         link_text = match.group(1)
-        ingredient_name = match.group(2)
-        ingredient_category = match.group(3)  # None if not specified
+        # The following may be None
+        parentheses_clause = match.group(2)
+        ingredient_name = match.group(3)
+        ingredient_category = match.group(4)
         
         try:
             # Import here to avoid circular imports
             from food_db_app.models import Ingredient
             from food_db_app.views import RecipeIngredientData
-            
-            # We don't want to require the specification of ingredient_category, even if the ingredient does have a category. So try to find it first without filtering on category.
-            try:
-                ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name)
-            except Ingredient.MultipleObjectsReturned:
-                ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name, ingredient_category__name=ingredient_category or '')
+
+            # First case is the ingredient name written just in brackets, like [ingredient name]
+            if not parentheses_clause:
+                ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=link_text)
+            else:
+                # In this case, both brackets and parenetheses were used
+                # Checking the second case: [visible text](!ingredient name)
+                # We don't want to require the specification of ingredient_category, even if the ingredient does have a category. So try to find it first without filtering on category.
+                try:
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name)
+                except Ingredient.MultipleObjectsReturned:
+                    # The same food was used for multiple ingredients in this recipe, so an ingredient category is required. The full syntax for specifying this is [visible text](!ingredient name;category name)
+                    cat = ingredient_category or ''  # if no category was specified, try searching with a blank string (which is what happens when no category is attached to the ingredient)
+                    cat = cat.replace(';','').strip()  # if a category was specified, remove leading whitespace and the semicolon from the regular expression match
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name, ingredient_category__name=cat)
                 
             quantity = RecipeIngredientData.stringify_ingredient_quantity(ingredient.quantity, ingredient.unit_of_measurement.name or '', 1)  # FIXME: gotta work dynamically with the multiplier, right now it's set to 1
             
