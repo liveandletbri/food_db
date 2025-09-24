4998da91aff63c2606cc5fa0ad1ca8e6507e4200
Brian Pohl <29556484+liveandletbri@users.noreply.github.com>
2025-09-24 04:15:40 +0100

    Automatically create ingredient links when adding a new recipe (#83)
    * add auto_parse
    
    * ignore case, remove auto_parse
    
    * auto parse

--- food_db/food_db_app/templatetags/markdown.py
+++ food_db/food_db_app/templatetags/markdown.py
@@ -10,10 +10,10 @@ register = template.Library()
 @stringfilter
 def markdown(value, recipe_title):
     # Process custom ingredient links before passing to markdown
-    processed_value = _process_ingredient_links(value, recipe_title)
+    processed_value = process_ingredient_links(value, recipe_title)
     return md.markdown(processed_value, extensions=['markdown.extensions.fenced_code'])
 
-def _process_ingredient_links(text, recipe_title):
+def process_ingredient_links(text, recipe_title):
     """
     Process custom ingredient links in the format [text](!ingredient name;ingredient category)
     and replace them with HTML spans that include tooltip data.
@@ -41,12 +41,12 @@ def _process_ingredient_links(text, recipe_title):
                 # Checking the second case: [visible text](!ingredient name)
                 # We don't want to require the specification of ingredient_category, even if the ingredient does have a category. So try to find it first without filtering on category.
                 try:
-                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name)
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=ingredient_name)
                 except Ingredient.MultipleObjectsReturned:
                     # The same food was used for multiple ingredients in this recipe, so an ingredient category is required. The full syntax for specifying this is [visible text](!ingredient name;category name)
                     cat = ingredient_category or ''  # if no category was specified, try searching with a blank string (which is what happens when no category is attached to the ingredient)
                     cat = cat.replace(';','').strip()  # if a category was specified, remove leading whitespace and the semicolon from the regular expression match
-                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name=ingredient_name, ingredient_category__name=cat)
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=ingredient_name, ingredient_category__name__iexact=cat)
                 
             quantity = RecipeIngredientData.stringify_ingredient_quantity(ingredient.quantity, ingredient.unit_of_measurement.name or '', 1)  # FIXME: gotta work dynamically with the multiplier, right now it's set to 1
             
@@ -61,12 +61,13 @@ def _process_ingredient_links(text, recipe_title):
             # Return HTML span with tooltip attributes
             return f'<span class="ingredient_link" data-tooltip="{tooltip_content}">{link_text}</span>'
             
-        except Ingredient.DoesNotExist:
-            # If ingredient doesn't exist, return the hyperlink text (without the link formatting around it) plus an error
-            return match.group(1) + ' <linked ingredient not found>'
-        except Ingredient.MultipleObjectsReturned:
-            return match.group(1) + ' <multiple linked ingredients found; try specifying category>'
-        except Exception:
-            return match.group(1) + ' <link error>'
+        except Exception as e:
+            if isinstance(e, Ingredient.DoesNotExist):
+                error_message = ' <linked ingredient not found>'
+            elif isinstance(e, Ingredient.MultipleObjectsReturned):
+                error_message = ' <multiple linked ingredients found; try specifying category>'
+            else:
+                error_message = ' <link error>'
+            return match.group(1) + error_message
     
-    return re.sub(pattern, replace_ingredient_link, text)
+    return re.sub(pattern, replace_ingredient_link, text, flags=re.IGNORECASE)
