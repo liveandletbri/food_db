2eb0d07d4a5604ebdf1104cb482d440f2fa2970f
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2025-10-20 21:36:34 -0700

    weird pytz bug

--- food_db/food_db_app/views/pages.py
+++ food_db/food_db_app/views/pages.py
@@ -1,4 +1,5 @@
 import json
+import pytz
 import re
 
 from collections import Counter, defaultdict, OrderedDict
@@ -8,7 +9,6 @@ from django.core.serializers import serialize
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from pytz import timezone
 
 from food_db_app.charts import AllCharts
 from food_db_app.filters import RecipeTextFilter, FoodTextFilter, FoodCategoryTextFilter
@@ -98,7 +98,7 @@ def recipe_detail(request, key):
 
     last_cooked_meal = CookedMeal.objects.filter(recipe=recipe).order_by('date_cooked').last()
     if last_cooked_meal:
-        last_cooked_date = last_cooked_meal.date_cooked.astimezone(timezone('America/Los_Angeles')).strftime('%b %d, %Y')
+        last_cooked_date = last_cooked_meal.date_cooked.astimezone(pytz.timezone('America/Los_Angeles')).strftime('%b %d, %Y')
     else:
         last_cooked_date = ''
 
@@ -369,7 +369,7 @@ def search(request):
     recipe_data = {recipe.title : {} for recipe in found_recipes}
     for recipe in found_recipes:
         recipe_data[recipe.title]['tags'] = [t['fields'] for t in json.loads(serialize('json',recipe.associated_tags))]  # convert Tag to JSON, then dict, because Tag object cannot be implicitly serialized into JSON (which is done on the search template so the data is accessible in Javascript layer)
-        recipe_data[recipe.title]['date_created'] = recipe._date_created.astimezone(timezone('America/Los_Angeles')).strftime('%b %d, %Y')
+        recipe_data[recipe.title]['date_created'] = recipe._date_created.astimezone(pytz.timezone('America/Los_Angeles')).strftime('%b %d, %Y')
         recipe_data[recipe.title]['clean_key'] = recipe.clean_key
         recipe_data[recipe.title]['duration_minutes'] = recipe.duration_minutes or 0
         recipe_data[recipe.title]['duration_string'] = convert_minutes_to_string(recipe.duration_minutes) if recipe.duration_minutes else ''
@@ -377,7 +377,7 @@ def search(request):
         last_cooked_meal = CookedMeal.objects.filter(recipe=recipe).order_by('date_cooked').last()
         if cooked_count > 0:
             recipe_data[recipe.title]['times_cooked'] = str(cooked_count)
-            recipe_data[recipe.title]['last_cooked'] = last_cooked_meal.date_cooked.astimezone(timezone('America/Los_Angeles')).strftime('%b %d, %Y')
+            recipe_data[recipe.title]['last_cooked'] = last_cooked_meal.date_cooked.astimezone(pytz.timezone('America/Los_Angeles')).strftime('%b %d, %Y')
         else:
             recipe_data[recipe.title]['times_cooked'] = ''
             recipe_data[recipe.title]['last_cooked'] = ''
