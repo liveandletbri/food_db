e4f5efe0a7d72e7efb65c9bd48cdce9a1ff730f1
liveandletbri <29556484+liveandletbri@users.noreply.github.com>
2026-01-28 20:52:00 -0800

    clear up the variables

--- food_db/food_db_app/templatetags/markdown.py
+++ food_db/food_db_app/templatetags/markdown.py
@@ -29,8 +29,8 @@ def process_ingredient_links(text, recipe_title, multiplier):
         link_text = match.group(1)
         # The following may be None
         parentheses_clause = match.group(2)
-        ingredient_name = match.group(3)
-        ingredient_category = match.group(4)
+        parsed_ingredient_name = match.group(3)
+        parsed_ingredient_category = match.group(4)
         
         try:
             # Import here to avoid circular imports
@@ -40,23 +40,25 @@ def process_ingredient_links(text, recipe_title, multiplier):
             # First case is the ingredient name written just in brackets, like [ingredient name]
             if not parentheses_clause:
                 ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=link_text)
+                ingredient_category = ingredient.ingredient_category or ''
             else:
                 # In this case, both brackets and parenetheses were used
                 # Checking the second case: [visible text](!ingredient name)
                 # We don't want to require the specification of ingredient_category, even if the ingredient does have a category. So try to find it first without filtering on category.
                 try:
-                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=ingredient_name)
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=parsed_ingredient_name)
                 except Ingredient.MultipleObjectsReturned:
                     # The same food was used for multiple ingredients in this recipe, so an ingredient category is required. The full syntax for specifying this is [visible text](!ingredient name;category name)
-                    cat = ingredient_category or ''  # if no category was specified, try searching with a blank string (which is what happens when no category is attached to the ingredient)
-                    cat = cat.replace(';','').strip()  # if a category was specified, remove leading whitespace and the semicolon from the regular expression match
-                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=ingredient_name, ingredient_category__name__iexact=cat)
+                    ingredient_category = parsed_ingredient_category or ''  # if no category was specified, try searching with a blank string (which is what happens when no category is attached to the ingredient)
+                    ingredient_category = ingredient_category.replace(';','').strip()  # if a category was specified, remove leading whitespace and the semicolon from the regular expression match
+                    ingredient = Ingredient.objects.get(recipe__title=recipe_title, food__name__iexact=parsed_ingredient_name, ingredient_category__name__iexact=ingredient_category)
                 
             quantity = RecipeIngredientData.stringify_ingredient_quantity(ingredient.quantity, ingredient.unit_of_measurement.name or '', multiplier)
+            ingredient_name = ingredient.food.name
+
+            tooltip_content = f'{quantity} {ingredient_name}'
             
-            tooltip_content = f'{quantity} {ingredient.food.name}'
-            
-            if ingredient.quantity != 1 and not ingredient.unit_of_measurement and not ingredient.food.name.endswith('s'):
+            if ingredient.quantity != 1 and not ingredient.unit_of_measurement and not ingredient_name.endswith('s'):
                 tooltip_content += 's'
             
             if ingredient.notes:
